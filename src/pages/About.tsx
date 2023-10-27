7ee93e227e23828f0677a8d6ebe33e60b3a6ade4
Alec Meyer <11meyal@gmail.com>
2023-10-27 16:17:43 -0400

    Gud

---
+++ src/pages/About.tsx
@@ -0,0 +1,19 @@
+export default function About() {
+    return (
+        <div className="about">
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda at autem culpa cumque
+                distinctio dolore, expedita fuga ipsum neque nihil odio officia praesentium quasi, quidem ratione sint
+                veniam, voluptatibus.
+            </p>
+        </div>
+    )
+}
